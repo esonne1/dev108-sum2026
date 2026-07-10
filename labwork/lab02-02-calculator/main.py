fa8714a915ddfb0de11964fdf547291e925989b4
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-09 21:40:38 -0700

    Implement area and perimeter calculations
    Added functionality to calculate area and perimeter based on user input.

--- labwork/lab02-02-calculator/main.py
+++ labwork/lab02-02-calculator/main.py
@@ -1,2 +1,18 @@
 # this is the starting file for Lab Activity 2.2
-# create your own starting code for the instructions provided 
+print("Area and Perimitter")
+print()
+# Get input from loser
+length += int(input("Please Enter Length: \t"))
+width += int(input("Please Enter Width: \t\t"))
+# Calculate the area and perimitter
+length = 25
+width = 10
+area = (length * width)
+perimitter = (length * 2)+(width * 2)
+area = round(area, 2)
+perimitter = round(perimitter, 2)
+print()
+print(f"Area: ", {area})
+print(f"Perimitter: ", {perimitter})
+print()
+print("Toodles")
