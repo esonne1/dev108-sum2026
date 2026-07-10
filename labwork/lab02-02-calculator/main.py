5b3daddea26cd1ad48042e7b11f988d4a6f40044
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-09 21:51:00 -0700

    Fix typos in print statements and variable names

--- labwork/lab02-02-calculator/main.py
+++ labwork/lab02-02-calculator/main.py
@@ -1,18 +1,18 @@
 # this is the starting file for Lab Activity 2.2
-print("Area and Perimitter")
+print("Area and Perimeter")
 print()
-# Get input from loser
+# Get input
 length += int(input("Please Enter Length: \t"))
 width += int(input("Please Enter Width: \t\t"))
-# Calculate the area and perimitter
+# Calculate the area and perimeter
 length = 25
 width = 10
 area = (length * width)
-perimitter = (length * 2)+(width * 2)
+perimeter = (length * 2)+(width * 2)
 area = round(area, 2)
-perimitter = round(perimitter, 2)
+perimeter = round(perimeter, 2)
 print()
 print(f"Area: ", {area})
-print(f"Perimitter: ", {perimitter})
+print(f"Perimeter: ", {perimeter})
 print()
 print("Toodles")
