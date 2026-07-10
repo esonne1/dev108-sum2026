a9dda9dcb09aadcce9442be03a349420ae2002ba
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-10 05:53:32 +0000

    Clean Final

--- labwork/lab02-02-calculator/main.py
+++ labwork/lab02-02-calculator/main.py
@@ -1,18 +1,20 @@
 # this is the starting file for Lab Activity 2.2
+print()
 print("Area and Perimeter")
+
 print()
 # Get input
-length += int(input("Please Enter Length: \t"))
-width += int(input("Please Enter Width: \t\t"))
+length_r = float(input("Please Enter Length: \t"))
+width_r = float(input("Please Enter Width: \t"))
+
 # Calculate the area and perimeter
-length = 25
-width = 10
-area = (length * width)
-perimeter = (length * 2)+(width * 2)
+area = (length_r * width_r)
+perimeter = (length_r * 2)+(width_r * 2)
 area = round(area, 2)
 perimeter = round(perimeter, 2)
+#format result
 print()
-print(f"Area: ", {area})
-print(f"Perimeter: ", {perimeter})
+print(f"Area: ", area)
+print(f"Perimeter: ", perimeter)
 print()
 print("Toodles")
