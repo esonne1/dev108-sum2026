3560eb8be59094a472144e2c8a024dfe573877f7
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-16 22:45:15 +0000

    first run

--- labwork/lab04-02-testscores/main.py
+++ labwork/lab04-02-testscores/main.py
@@ -5,18 +5,20 @@
 # display a welcome message
 print("The Test Scores application")
 print()
-print("Enter test scores")
-print("Enter 999 to end input")
-print("======================")
+repeat_tests = "y"
+    while repeat_tests.lower() == "y":  
+    print("Enter test scores")
+    print("Enter "end" to end input")
+    print("======================")
 
 # initialize variables
 counter = 0
 score_total = 0
 test_score = 0
 
-while test_score != 999:
-    test_score = int(input("Enter test score: "))
-    if test_score >= 0 and test_score <= 100:
+    while True:
+      test_score = (input("Enter test score: "))
+      if test_score >= 0 and test_score <= 100:
         score_total += test_score
         counter += 1
     elif test_score == 999:
