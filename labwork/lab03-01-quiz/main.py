12fe4bb7dadddf8c6da067d08506a3f9cd00db25
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-16 20:52:51 +0000

    final mpg

--- labwork/lab03-01-quiz/main.py
+++ labwork/lab03-01-quiz/main.py
@@ -1,1 +1,70 @@
-# Create a simple 5 question math or multiple choice quiz following the instructions provided
+# Create a simple 5 question math or multiple choice quiz following th
+print("Math Quiz")
+print()
+# ask if they want to play
+ans = input("Would you like to take a math quiz?y/n ")
+if ans.lower() == "y":
+    print("Okay, let's play!")
+elif ans.lower() == "n":
+    print("Sorry. Maybe later.")
+else:
+    print("Invalid entry. Please restart")
+# Math Questions
+# initialize a counter
+counter = 0
+print()
+q1 = int(input("Math Question 1: What is 5 X 5? "))
+if q1 == 25:
+    print("Good job! You are right")
+    counter += 1
+else:
+    print("Sorry, that is wrong. The answer was 25.")
+q2 = int(input("Math Question 2: What is 150/3? "))
+if q2 == 50:
+    print("Good job! You are right")
+    counter += 1
+else:
+    print("Sorry, that is wrong. The answer was 50.")
+q3 = int(input("Math Question 3: What is 33 X 10? "))
+if q3 == 330:
+    print("Good job! You are right")
+    counter += 1
+else:
+    print("Sorry, that is wrong. The answer was 330.")
+q4 = int(input("Math Question 4: What is 21/3? "))
+if q4 == 7:
+    print("Good job! You are right")
+    counter += 1
+else:
+    print("Sorry, that is wrong. The answer was 7.")
+q5 = int(input("Math Question 5: What is 40+50+60? "))
+if q5 == 150:
+    print("Good job! You are right")
+    counter += 1
+else:
+    print("Sorry, that is wrong. The answer was 150.")
+print("Your score is: ",counter)
+print("Thanks for playing")
+if counter == 5:
+    print("You are a rock start!")
+elif counter == 4:
+    print("Great job!")
+elif counter == 3:
+    print("Almost there!")
+elif counter == 2:
+    print("Keep studying")
+elif counter == 1:
+    print("Don't give up!")
+
+
+
+
+
+
+
+
+
+
+
+
+
--- labwork/lab04-01-mpg/main.py
+++ labwork/lab04-01-mpg/main.py
@@ -4,20 +4,30 @@
 print("The Miles Per Gallon application")
 print()
 
-# get input from the user
-miles_driven = float(input("Enter miles driven:         "))
-gallons_used = float(input("Enter gallons of gas used:  "))
-
-if miles_driven <= 0:
-    print("Miles driven must be greater than zero. Please try again.")
-elif gallons_used <= 0:
-    print("Gallons used must be greater than zero. Please try again.")
-else:
-    # inputs must be good
-    # calculate and display miles per gallon
-    mpg = round((miles_driven / gallons_used), 2)
-    print("Miles Per Gallon:          ", mpg)
-
-print()
+repeat_trip = "y"
+while repeat_trip =="y":
+    
+    # get input from the user
+    miles_driven = float(input("Enter miles driven:         "))
+    gallons_used = float(input("Enter gallons of gas used:  "))
+    cost_per_gallon = float(input("Enter cost per gallon:   "))
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+    elif cost_per_gallon <= 0:
+        print("Cost per gallon must be greater than zero. Please try again.")
+    else:
+    # calculate and display 
+        mpg = round((miles_driven / gallons_used), 2)
+        total_gas_cost = round((gallons_used * cost_per_gallon), 2)
+        cost_per_mile = round((total_gas_cost / miles_driven), 2)
+        print()
+        print("Miles Per Gallon:          ", mpg)
+        print("Total Gas cost:            ", total_gas_cost)
+        print("Cost per Mile:             ", cost_per_mile)
+        print()
+        repeat_trip = input("Get entries for another trip (y/n)? ")
+        print()
 print("Bye")
 
