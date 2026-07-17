fbb15852ddc249b287fc5098fbfa3c3da83e2361
esonne1 <ethan.sonneborn@bellevuecollege.edu>
2026-07-17 16:25:04 +0000

    FINAL TEST

--- labwork/lab04-02-testscores/main.py
+++ labwork/lab04-02-testscores/main.py
@@ -6,44 +6,37 @@ print("The Test Scores Applicant")
 more_tests = "y"
 
 while more_tests.lower() == "y":
-    print("Enter test scores")
-    print("Enter 'end' to end input")
-    print("======================")
+  print("Enter test scores")
+  print("Enter 'end' to end input")
+  print("======================")
 # initialize variables
-    counter = 0
-    score_total = 0
-    test_score = 0
+  counter = 0
+  score_total = 0
+  test_score = 0
 
-##    while True:
-##        test_score = input("Enter test score: ")
-##        if test_score.lower() == "end":
-##            break
-##        else:
-##            test_score = int(test_score)
-##            if test_score >= 0 and test_score <= 100:
-##                score_total += test_score
-##                counter += 1
-##            else:
-##                print(f"Test score must be from 0 through 100. "
-##                      f"Score discarded. Try again.")
-
-    while (test_score := input("Enter test score: ").lower()) != "end":
-        test_score = int(test_score)
-        if test_score >= 0 and test_score <= 100:
-            score_total += test_score
-            counter += 1
-        else:
-            print(f"Test score must be from 0 through 100. "
-                  f"Score discarded. Try again.")
+  while test_score != 999:
+    test_score = input("enter test score or end to end input: ")
+    if test_score.lower() == "end":
+      break
+    else:
+         # change input to integer
+      test_score = int(test_score)
+      if test_score >= 0 and test_score <= 100:
+        score_total += test_score
+        counter += 1
+      else:
+        print(f"Test score must be from 0 through 100. "
+              f"Score discarded. Try again.")
             
     # calculate average score
-    average_score = round(score_total / counter)
+  average_score = round(score_total / counter)
                     
     # format and display the result
-    print("======================")
-    print(f"Total Score: {score_total}"
+  print("======================")
+  print(f"Total Score: {score_total}"
           f"\nAverage Score: {average_score}")
 
-    print()
-    more_tests = input("Enter another set of test scores (y/n)? ") 
-    print()
+  print()
+  more_tests = input("Enter another set of test scores (y/n)? ") 
+  print()
+print("Goodbye")
